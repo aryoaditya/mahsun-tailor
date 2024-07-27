3aad9a3cadc34955939a1456b218c2a8b47712c3
Aryo Aditya <aryoaditya2000@gmail.com>
2024-07-28 00:53:00 +0700

    style: footer ui adjustment

--- src/components/Fragments/Footers/Footer.js
+++ src/components/Fragments/Footers/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
       <hr className="w-full border-stone-200 mx-auto mb-7" />
       {/* Footer Content under hr line */}
       <div className="flex flex-col items-center w-full px-3  sm:flex-row sm:justify-between sm:items-center sm:px-10 md:px-14 lg:px-16">
-        <div className="w-2/3 text-xs text-center sm:text-left">
+        <div className="flex flex-col gap-1 w-2/3 text-xs text-center sm:text-left">
           <p className="font-bold">Hubungi kami:</p>
-          <p className="font-normal">081312387123</p>
-          <p className="font-normal">
+          <p className="font-normal sm:pl-2">081312387123</p>
+          <p className="font-normal sm:pl-2">
             Jl. Jalan Cipinang Muara, Jatinegara, Jakarta Timur.
           </p>
         </div>
